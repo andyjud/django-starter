5621159f7720a34fbb09bcd196170fbb87e2051c
andyjud <andreas.jud@gmail.com>
2023-04-05 09:38:11 +0800

    Update urls.py

--- a_core/urls.py
+++ a_core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from a_home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', home_view, name='home'),
 ]
