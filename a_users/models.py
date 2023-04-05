869fa23eb87b9d15e30e6049ea1b7d8981204d46
andyjud <andreas.jud@gmail.com>
2023-04-05 17:51:47 +0800

    Update models.py

--- a_users/models.py
+++ a_users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(unique=True, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
     
     def __str__(self):
         return str(self.user)
