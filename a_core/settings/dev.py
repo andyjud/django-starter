a021ebbb2698aa0275d1c8223b4880f37e11e7c1
Taha Khidr <tahakhidr@gmail.com>
2024-03-22 16:53:32 +0300

    feature: Integrate dotenv for environment management and separate settings
    - Added dotenv support for managing environment variables.
    - Split settings into dev.py and prod.py for clearer separation between development and production configurations.
    - Configured prod.py to use MySQL database settings from .env.

---
+++ a_core/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
--- manage.py
+++ manage.py
@@ -3,10 +3,15 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "a_core.settings")
+
+    load_dotenv()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +23,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
