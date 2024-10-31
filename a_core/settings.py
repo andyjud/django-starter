a6a9299539682fd42e12d42607f5ca3d65fad048
Jonatan Snyders <jonatansnyders@gmail.com>
2024-10-31 10:22:42 +0100

    Fixed reading .env in settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -34,12 +34,13 @@ env = Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rj#-z^kx3j+1ay397otg6j8m_8#v^$^$jys6&41vy^&6le)ezc'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ENVIRONMENT = env('ENVIRONMENT')
