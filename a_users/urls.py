3d9e7c3d155fd67b3d0f8c7d3e85331b1dc48d8b
Andreas Jud <andreas.jud@gmail.com>
2025-02-13 04:59:45 +0100

    Update urls.py

--- a_users/urls.py
+++ a_users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('onboarding/', profile_edit_view, name="profile-onboarding"),
     path('settings/', profile_settings_view, name="profile-settings"),
     path('emailchange/', profile_emailchange, name="profile-emailchange"),
+    path('usernamechange/', profile_usernamechange, name="profile-usernamechange"),
     path('emailverify/', profile_emailverify, name="profile-emailverify"),
     path('delete/', profile_delete_view, name="profile-delete"),
 ]
