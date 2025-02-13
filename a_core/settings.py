d825b9414059886ce67b9ce50c2db1f80f674fd4
Andreas Jud <andreas.jud@gmail.com>
2025-02-13 05:54:03 +0100

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -147,7 +147,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
-ACCOUNT_SIGNUP_REDIRECT_URL = "{% url 'account_signup' %}?next={% url 'profile-onboarding' %}"
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_LOGIN_METHODS = {'email', 'username'}
