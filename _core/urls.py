20cc2cf016e0179c9b73a4c067116747952f0880
andyjud <andreas.jud@gmail.com>
2026-03-31 16:07:38 +0200

    Update urls.py

--- _core/urls.py
+++ _core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from a_home.views import home_view
+from a_home.views import *
 from a_users.views import profile_view
 
 urlpatterns = [
