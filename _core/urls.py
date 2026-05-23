9f58d8f3c1c575bd6e394064ebc09b91359e3c5d
kramstyles <kramstyles@outlook.com>
2026-05-23 23:11:53 +0100

    fixed debug toolbar

--- _core/urls.py
+++ _core/urls.py
@@ -21,6 +21,10 @@ from django.conf import settings
 from a_home.views import *
 from a_users.views import profile_view
 
+admin.site.site_header = f"{settings.UNFOLD['SITE_TITLE']} Admin Dashboard"
+admin.site.site_title = settings.UNFOLD['SITE_HEADER']
+admin.site.index_title = "Last Code Update: 20th May 26 22:00"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -29,9 +33,11 @@ urlpatterns = [
     path('@<username>/', profile_view, name="profile"),
 ]
 
-# Only used in development
-if settings.DEBUG:
+# Only used in development, whitenoise can serve files when DEBUG=False
+if settings.DEBUG and settings.ENVIRONMENT == 'development':
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [
         path("__reload__/", include("django_browser_reload.urls")),
     ]
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    urlpatterns = urlpatterns + debug_toolbar_urls()
