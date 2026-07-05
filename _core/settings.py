dde06718ac5ae16e8594fd225c08de2728688e45
andyjud <andreas.jud@gmail.com>
2026-07-05 06:32:56 +0200

    Update settings.py

--- _core/settings.py
+++ _core/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'django-insecure-rj#-z^kx3j+1ay397otg6j8m_8#v^$^$jys6&41vy^&6le)ezc
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+
+CSRF_TRUSTED_ORIGINS = ['https://*']
 
 # Application definition
 
